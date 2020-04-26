d8c2f1734aff36d17506e986842b4561e94805fd
Teatoller <steven.otieno1@gmail.com>
2020-04-26 15:42:01 +0300

    (arguments): Implement args input field
    - add an args field
    - update gitignore file
    - update readme file
    
    [Starts #003]

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# gradebook
+# Gradebook
+
+An electronic grade book to read the scores of an individual student and then compute some simple statistics from the scores(highest, lowest and the average grade)
--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -6,7 +6,13 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Steven!");
+            if(args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]}!");
+            }
+            else {
+                Console.WriteLine("Hello!");
+            }
         }
     }
 }
