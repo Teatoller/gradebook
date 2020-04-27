56f38e376b25b7f000f11c3bd40e2a89fc1847ec
Teatoller <steven.otieno1@gmail.com>
2020-04-27 18:05:31 +0300

    (test): Implement identical variable test
    - test that two variables can reference same object
    
    [Starts #010]

--- test/Gradebook.Tests/TypeTests.cs
+++ test/Gradebook.Tests/TypeTests.cs
@@ -18,6 +18,23 @@ namespace Gradebook.Tests
             // assert
             Xunit.Assert.Equal("Book 1", book1.Name);
             Xunit.Assert.Equal("Book 2", book2.Name);
+            Assert.NotSame(book1, book2);
+
+        }
+
+        [Fact]
+        public void TwoVarsCanReferenceSameObjects()
+        {
+            // arrage
+
+            var book1 = GetBook("Book 1");
+            var book2 = book1;
+
+            // act
+
+            // assert
+            Assert.Same(book1, book2);
+            Assert.True(Object.ReferenceEquals(book1, book2));
 
         }
 
